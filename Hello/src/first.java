8c6d8b39ed968b06cdb0aa33dd55f3d6307ebeb3
Administrator <Administrator@A-201703101658>
2017-07-11 21:04:33 +0800

    d

--- Hello/src/first.java
+++ Hello/src/first.java
@@ -4,6 +4,8 @@ public class first {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I have a dream");
+		System.out.println("https://github.com/werfeng/first.git");
 	}
 	int e=3;
+	int a=1;
 }
