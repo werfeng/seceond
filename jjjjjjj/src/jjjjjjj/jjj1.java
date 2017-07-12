deb82ecbefe7cb50bc750d9b63c6060e2adf0fab
Administrator <666@163.com>
2017-07-12 22:31:22 +0800

    创建

---
+++ jjjjjjj/src/jjjjjjj/jjj1.java
@@ -0,0 +1,8 @@
+
+package jjjjjjj;
+
+public class jjj1 {
+	public static void main(String[] args) {
+		System.out.println("weewew");
+	}
+}
---
+++ jjjjjjj/src/jjjjjjj/tow.java
@@ -0,0 +1,8 @@
+
+package jjjjjjj;
+
+public class tow {
+	public static void main(String[] args) {
+		int o=0;
+	}
+}
